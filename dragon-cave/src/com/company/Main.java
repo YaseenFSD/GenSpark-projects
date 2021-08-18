a0574aa44304562f927a42bf36ba358420e3412d
Yaseen <yhalsalamy@gmail.com>
2021-08-18 15:39:37 -0400

    program is now functional and randomly chooses cave

--- dragon-cave/src/com/company/Main.java
+++ dragon-cave/src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
+import java.util.Random;
 
+public class Main {
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("You are in a land full of dragons. In front of you,");
+        System.out.println("you see two caves. In one cave, the dragon is friendly");
+        System.out.println("and will share his treasure with you. The other dragon");
+        System.out.println("is greedy and hungry and will eat you on sight.");
+        System.out.println("Which cave will you go into? (1 or 2)");
+
+        int input = sc.nextInt();
+        Random rand = new Random();
+        int randInt = rand.nextInt(2) + 1;
+        if (randInt == input){
+            goodCave();
+        } else {
+            badCave();
+        }
+    }
+    public static void goodCave() {
+        System.out.println("It is dark and spooky...");
+        System.out.println("You approach the cave...");
+        System.out.println("You softly hear a voice and...");
+        System.out.println("Then you get cuddled by the dragon 🥰");
+
+    }
+    public static void badCave() {
+        System.out.println("You approach the cave...");
+        System.out.println("It is dark and spooky...");
+        System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+        System.out.println("Gobbles you down in one bite!");
     }
 }

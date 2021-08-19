dac36bcb8c1e3e2b883fd7110d0305420fd07608
yaseen <yhalsalamy@gmail.com>
2021-08-19 09:12:57 -0400

    program now saves user's name

--- guess-the-number/src/com/company/Main.java
+++ guess-the-number/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! What is your name?");
+        String username = sc.next();
+        System.out.printf("Hello %s, guess a number between 1 and 20\n", username);
+
     }
 }

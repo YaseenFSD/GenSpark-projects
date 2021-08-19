d60021f8f7de825c3b69074f9e1ec64fc3ba4881
Yaseen <yhalsalamy@gmail.com>
2021-08-19 15:13:53 -0400

    printHangman accepts a stage level and prints corresponding hangman

--- hangman/src/com/company/Main.java
+++ hangman/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.Random;
 
 /* ascii art credit: https://ascii.co.uk/art/hangman
@@ -18,10 +19,89 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getRandomWord());
+
     }
 
-    public static String getRandomWord() {
+    static String getRandomWord() {
         int randInt = new Random().nextInt(words.length);
         return words[randInt];
     }
+
+    static void printHangman(int stage) {
+//        Stage 0 is no mistakes have been made yet. Stage 5 is the last stage
+
+//        Concise Solution
+        /*
+        System.out.println("      _______");
+        System.out.println("     |/      " + (stage > 0 ? "|" : ""));
+        System.out.println("     |      " + (stage > 1 ? "(_)" : ""));
+        System.out.println("     |      " + (stage > 2 ? "\\|/" : ""));
+        System.out.println("     |       " + (stage > 3 ? "|" : ""));
+        System.out.println("     |      " + (stage > 4 ? "/ \\" : ""));
+        System.out.println("     |");
+        System.out.println(" ____|___");
+        */
+
+//        Performance Solution
+//        This is faster because of less condition checks
+        System.out.println("      _______");
+
+        switch (stage) {
+            case 1:
+                System.out.print(
+                        "     |/      |\n" +
+                                "     |      \n" +
+                                "     |      \n" +
+                                "     |       \n" +
+                                "     |      \n");
+
+
+                break;
+            case 2:
+                System.out.print("     |/      |\n"
+                        + "     |      (_)\n"
+                        + "     |      \n"
+                        + "     |       \n"
+                        + "     |      \n");
+
+                break;
+            case 3:
+                System.out.println("     |/      |\n"
+                        + "     |      (_)\n"
+                        + "     |      \\|/\n"
+                        + "     |       \n"
+                        + "     |      \n");
+
+
+                break;
+            case 4:
+                System.out.print("     |/      |\n"
+                        + "     |      (_)\n"
+                        + "     |      \\|/\n"
+                        + "     |       |\n"
+                        + "     |      \n");
+
+                break;
+            case 5:
+                System.out.print("     |/      |\n"
+                        + "     |      (_)\n"
+                        + "     |      \\|/\n"
+                        + "     |       |\n"
+                        + "     |      / \\\n");
+
+                break;
+
+            default:
+                System.out.print("     |/      \n"
+                        + "     |      \n"
+                        + "     |      \n"
+                        + "     |       \n"
+                        + "     |      \n");
+                break;
+        }
+        System.out.println("     |");
+        System.out.println(" ____|___");
+
+    }
+
 }

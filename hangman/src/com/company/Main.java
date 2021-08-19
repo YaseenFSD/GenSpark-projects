3b757f22df81e6f67f4495388c7e9d405425a2dd
Yaseen <yhalsalamy@gmail.com>
2021-08-19 13:33:03 -0400

    Initial hangman setup

---
+++ hangman/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/* ascii art credit: https://ascii.co.uk/art/hangman
+      _______
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      / \
+     |
+ ____|___
+         */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}

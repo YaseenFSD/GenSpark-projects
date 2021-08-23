4fe14e8bf57b87094d5c62204230e473bc899486
Yaseen <yhalsalamy@gmail.com>
2021-08-22 21:56:17 -0400

    slight refactors (currentGuess is now an Arraylist)

--- hangman/src/com/company/Hangman.java
+++ hangman/src/com/company/Hangman.java
@@ -1,9 +1,7 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Hangman {
     private static final String[] words = {"banana", "apple", "monkey", "orange", "cat", "dog", "one", "monster"};
@@ -11,51 +9,51 @@ public class Hangman {
     private int stage = 0;
     private String answer;
     ArrayList<Character> historyLetters = new ArrayList<>();
+    ArrayList<Character> currentGuess = new ArrayList<>();
     boolean gameDone = false;
     boolean roundDone = false;
 
     public void start(Scanner sc) {
         answer = getRandomWord();
-        char[] currentGuess = getFillerArray();
-        while (!gameDone){
+//        char[] currentGuess = initializeArray();
+        initializeCurrentGuess();
+        while (!gameDone) {
             printHangman();
-            printLetters(currentGuess);
+            printLetters();
             char input = sc.next().charAt(0);
             if (hasLetter(input)) {
-                updateGuess(currentGuess, input);
-            }else {
-                if (checkLetterMistake(input)){
+                updateGuess(input);
+            } else {
+                if (checkLetterMistake(input)) {
                     stage++;
                 }
             }
 
-            char[] answerCharArray = answer.toCharArray();
-            boolean wonGame = Arrays.equals(answerCharArray, currentGuess);
-            if (wonGame){
+//            char[] answerCharArray = answer.toCharArray();
+
+//            boolean wonGame = Arrays.equals(answerCharArray, currentGuess);
+            String guessStringRep = currentGuess.stream().map(x -> x.toString()).reduce("", (acc, val) -> acc + val);
+            boolean wonGame = guessStringRep.equals(answer);
+            if (wonGame) {
                 System.out.println("Congratulations, you won!");
                 roundDone = true;
             }
 
             if (stage > 4 || roundDone) {
-                if (!wonGame){
+                if (!wonGame) {
                     printHangman();
                     System.out.println("Oh no hangman is gone for good. 😢");
                 }
                 if (wantsRestart(sc)) {
                     restartGame();
-                    currentGuess = getFillerArray();
-//                    gameDone = false;
-//                    roundDone = false;
-//                    answer = getRandomWord();
-//                    stage = 0;
-//                    currentGuess = getFillerArray(answer.length());
-//                    historyLetters.clear();
-                }else {
+                    initializeCurrentGuess();
+                } else {
                     gameDone = true;
                 }
             }
         }
     }
+
     void restartGame() {
         gameDone = false;
         roundDone = false;
@@ -63,7 +61,8 @@ public class Hangman {
         stage = 0;
         historyLetters.clear();
     }
-    boolean wantsRestart(Scanner sc){
+
+    boolean wantsRestart(Scanner sc) {
         System.out.print("Do you want to play again? (y or n)");
         char input = sc.next().charAt(0);
         if (input == 'y') {
@@ -76,22 +75,23 @@ public class Hangman {
         if (historyLetters.contains(input)) {
             System.out.printf("You have already tried ' %c '\n", input);
             return false;
-        };
+        }
+        ;
 
         historyLetters.add(input);
         return true;
     }
 
-    void updateGuess(char[] currentGuess, char correctChar){
+    void updateGuess(char correctChar) {
         for (int i = 0; i < answer.length(); i++) {
             char currentLetter = answer.charAt(i);
             if (correctChar == currentLetter) {
-                currentGuess[i] = currentLetter;
+                currentGuess.set(i, currentLetter);
             }
         }
     }
 
-    boolean hasLetter(char letter){
+    boolean hasLetter(char letter) {
         boolean hasLetter = answer.contains(Character.toString(letter));
         if (hasLetter) {
             return true;
@@ -99,11 +99,12 @@ public class Hangman {
         return false;
     }
 
-    void printLetters(char[] guessedLetters){
+    void printLetters() {
         System.out.println("Guess a Letter");
-        for (char letter : guessedLetters) {
-            System.out.print(letter);
-        }
+//        for (char letter : guessedLetters) {
+//            System.out.print(letter);
+//        }
+        currentGuess.forEach(letter -> System.out.print(letter));
         System.out.println("");
 
         if (!historyLetters.isEmpty()) System.out.println("Missed Letters");
@@ -119,13 +120,14 @@ public class Hangman {
         return words[randInt];
     }
 
-    char[] getFillerArray() {
-        char[] fillers = new char[answer.length()];
-        Arrays.fill(fillers, '_');
-        return fillers;
+    void initializeCurrentGuess() {
+        currentGuess.clear();
+        for (int i = 0; i < answer.length(); i++) {
+            currentGuess.add('_');
+        }
     }
 
-    void printHangman(){
+    void printHangman() {
         //        Stage 0 is no mistakes have been made yet. Stage 5 is the last stage
 
 //        Concise Solution

60bb8d093fbeb83ceb15ebc1b2ec76eec5b3ab98
Yaseen <yhalsalamy@gmail.com>
2021-08-31 14:11:09 -0400

    Game now assigns names

--- battleship/src/com/company/Game.java
+++ battleship/src/com/company/Game.java
@@ -1,7 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Game {
-    void start(){
+    void start() {
+        Scanner sc = new Scanner(System.in);
 //        initializes game and assigns boards to each player
+        PlayerBoard playerOne = new PlayerBoard(sc);
+        PlayerBoard playerTwo = new PlayerBoard(sc);
+
+        playerOne.askName("player one");
+        playerTwo.askName("player two");
     }
 }
--- battleship/src/com/company/PlayerBoard.java
+++ battleship/src/com/company/PlayerBoard.java
@@ -1,12 +1,30 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PlayerBoard {
+    private Scanner sc;
+    private String name;
     private int maxX = 9;
     private int maxY = 9;
     private int[][] cells = new int[maxX][maxY];
     private ArrayList<Ship> playerShips = new ArrayList<>();
+
+    PlayerBoard  () {
+
+    }
+
+    PlayerBoard (Scanner sc) {
+        this.sc = sc;
+//        System.out.println()
+    }
+
+    void askName(String player){
+        System.out.println("Hi " + player + ", what is your name");
+        String input = sc.next();
+        this.name = input;
+    }
 
     void insertShip(Ship ship) {
 //

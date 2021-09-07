2e540e2bee77484f78c9f3f79dc370a19c0862b5
Yaseen <yhalsalamy@gmail.com>
2021-09-07 14:10:02 -0400

    initial setup for caesar cipher

---
+++ caesar-cipher/src/com/company/Crypt.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.File;
+
+public class Crypt {
+    public void encrypt(String message){
+
+    }
+
+    public String decrypt(String filename, int key){
+        return "";
+    }
+}

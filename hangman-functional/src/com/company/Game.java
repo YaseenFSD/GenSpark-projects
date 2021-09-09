666ec56e1486a3dbd11ec0f740fe6bf2dccf197a
Yaseen <yhalsalamy@gmail.com>
2021-09-09 11:15:10 -0400

    Initial setup

---
+++ hangman-functional/src/com/company/Game.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Game {
+    boolean gameIsDone = false;
+    String currentWord = "";
+    void start(){
+//        ask player for name
+    }
+
+    void play(){
+//        print current stage and current letters guessed
+//          printMan()
+//          printCurrentGuess()
+//          checkEnd()
+    }
+
+    String generateRandomWord(){
+        return "";
+    }
+}
---
+++ hangman-functional/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Game game = new Game();
+        game.start();
+        while (!game.gameIsDone) {
+//
+        }
+    }
+}
